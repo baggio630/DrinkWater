b6882343d0892fb3513faf23e3d37aeb6c7d2649
MetalKJ <baggio630@gmail.com>
2022-06-08 23:33:55 +0800

    1.0

--- DrinkWater/ContentView.swift
+++ DrinkWater/ContentView.swift
@@ -7,16 +7,64 @@
 
 import SwiftUI
 
+ 
+
 struct ContentView: View {
+    @State var isWatered = true
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack {
+                Image("background")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                Image(systemName: isWatered ? "hands.clap.fill" : "flame.fill")
+                    .font(.system(size: 130))
+                    .foregroundColor(isWatered ? .yellow : .red)
+            }
+            .frame(minHeight: 600)
+            .shadow(radius: 10)
+            Divider()
+            
+            Toggle(isOn: $isWatered) {
+                Label("Is you watered?", systemImage: "drop.fill")
+            }
+            .foregroundColor(.blue)
         }
+        .padding()
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
