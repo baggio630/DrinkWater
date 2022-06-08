88a35e17c921e5110fa9c93a1f0b90a37f01b826
MetalKJ <baggio630@gmail.com>
2022-06-08 23:42:19 +0800

    Created a new branch.Added to test branch.

--- DrinkWater/ContentView.swift
+++ DrinkWater/ContentView.swift
@@ -38,36 +38,10 @@ struct ContentView: View {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+// test commit to test
