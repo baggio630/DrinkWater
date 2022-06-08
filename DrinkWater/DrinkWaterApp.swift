a3475765f0a08b8754e2642d768529fd76be06cd
MetalKJ <baggio630@gmail.com>
2022-06-08 22:10:00 +0800

    Initial Commit

---
+++ DrinkWater/DrinkWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkWaterApp.swift
+//  DrinkWater
+//
+//  Created by Metal_KJ on 2022/6/8.
+//
+
+import SwiftUI
+
+@main
+struct DrinkWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
